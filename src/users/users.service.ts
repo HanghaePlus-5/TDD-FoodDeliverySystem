193e9219c8e85ba9e692a9b8fc8ff103aedac467
ninthsun91 <ninthsun91@gmail.com>
2023-06-21 03:49:03 +0900

    :white_check_mark: user signup - check duplication
    - pass - should return a User if user exists

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma';
 import { UserType } from 'src/types';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
   isUserType(userType: string|undefined): userType is UserType {
     return Object.values(UserType).includes(userType as UserType);
+  }
+
+  findUserByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
   }
 }
