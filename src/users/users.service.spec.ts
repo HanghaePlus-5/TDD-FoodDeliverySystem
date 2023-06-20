5956c95f8d1cb1cdde1c74b231bc3afc520763e5
ninthsun91 <ninthsun91@gmail.com>
2023-06-21 03:18:45 +0900

    :test_tube: user signup - check duplication
    - add failing test
    - should return a User if user exists

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,13 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { UserType } from 'src/types';
+import { PrismaService } from 'src/prisma';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  interface Select {
+    where: {
+      email: string;
+    };
+  }
+  const testUser = {
+    userId: 1,
+    email: 'test1@delivery.com',
+    name: 'Test Kim',
+    password: 'qwe1234',
+    type: 'customer',
+  }
+  const mockPrismaService = {
+    user: {
+      findUnique: jest.fn((select: Select) => {
+        return select.where.email === testUser.email ? testUser : null;
+      }),
+    }
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        PrismaService,
+      ],
+    }).useMocker((token) => {
+      if (token === PrismaService) {
+        return mockPrismaService;
+      }
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -18,6 +46,12 @@ describe('UsersService', () => {
   });
 
   describe('User Signup', () => {
+    const signupForm = {
+      email: 'test1@delivery.com',
+      name: 'Test Kim',
+      password: 'qwe1234',
+    }
+
     describe('Check UserType', () => {
       it('should return false if UserType is undefined.', () => {
         const userType = undefined;
@@ -39,7 +73,14 @@ describe('UsersService', () => {
       });
     });
 
-    describe('Check user duplication', () => {});
+    describe('Check user duplication', () => {
+      it('should return a User if user exists.', () => {
+        const result = service.findUserByEmail(testUser.email);
+        expect(result).toEqual(testUser);
+      });
+
+      it.todo('should return null if user does not exist.');
+    });
 
     describe('Encrypt user password', () => {});
 
