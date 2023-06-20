b052f37b62ca23e0cf814ac1a6b08fd73632f6f0
ninthsun91 <ninthsun91@gmail.com>
2023-06-21 02:47:54 +0900

    :wastebasket: delete checkUserType(deprecated)
    - isUserType에 의해 기능이 대체됨

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,6 @@ import { UserType } from 'src/types';
 
 @Injectable()
 export class UsersService {
-  checkUserType(userType: string|undefined): boolean {
-    return (
-      userType !== undefined
-      && ['customer', 'business'].includes(userType)
-    );
-  }
   isUserType(userType: string|undefined): userType is UserType {
     return Object.values(UserType).includes(userType as UserType);
   }
