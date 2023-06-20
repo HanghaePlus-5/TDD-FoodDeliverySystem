615e8fa01667a80fef42d8af94dc7ec36ac11609
ninthsun91 <ninthsun91@gmail.com>
2023-06-20 16:10:11 +0900

    :sparkles: add global ValidationPipe
    - add global ValidationPipe
    - set whitelist, forbiNonWhitelisted, transform

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [],
+  imports: [TestModule],
   controllers: [],
   providers: [],
 })
