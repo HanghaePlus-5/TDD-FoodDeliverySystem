8c7dae929cb4620cbb01c1058a31acb29ff29d2a
ninthsun91 <ninthsun91@gmail.com>
2023-06-23 03:17:30 +0900

    :white_check_mark: add test dependency in UserController

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PrismaService } from 'src/prisma';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,7 +9,10 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        PrismaService,
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
