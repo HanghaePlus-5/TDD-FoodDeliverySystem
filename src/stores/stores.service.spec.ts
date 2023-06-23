1aca6cdaced29e8ab547c278c805a283276a9577
Louis-Gil <lotus0110@nate.com>
2023-06-23 23:46:14 +0900

    :test_tube: add createStore it list
    5 list of createStore

--- src/stores/stores.service.spec.ts
+++ src/stores/stores.service.spec.ts
@@ -15,4 +15,17 @@ describe('StoresService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+    
+  describe('createStore', () => {
+    it('should check user type', async () => {});
+
+    it('should check validation', async () => {});
+
+    it('should check store name duplication', () => {});
+
+    it('should check store business number', () => {});
+
+    it('should delegate Store creation to repository', () => {});
+  });
 });
