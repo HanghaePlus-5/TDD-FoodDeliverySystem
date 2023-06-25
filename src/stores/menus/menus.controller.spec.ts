640c99cd9e27c95b55384aeef79465cbab5e209c
Louis-Gil <lotus0110@nate.com>
2023-06-25 23:38:07 +0900

    :rotating_light: use lint

--- src/stores/menus/menus.controller.spec.ts
+++ src/stores/menus/menus.controller.spec.ts
@@ -1,18 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { MenusController } from './menus.controller';
 
 describe('MenusController', () => {
-  let controller: MenusController;
+    let controller: MenusController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [MenusController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [MenusController],
+        }).compile();
 
-    controller = module.get<MenusController>(MenusController);
-  });
+        controller = module.get<MenusController>(MenusController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
 });
--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
-import { StoresController } from './stores/stores.controller';
-import { StoresService } from './stores/stores.service';
+
 import { MenusController } from './menus/menus.controller';
 import { MenusService } from './menus/menus.service';
+import { StoresController } from './stores/stores.controller';
+import { StoresService } from './stores/stores.service';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [StoresController, MenusController],
-  providers: [StoresService, MenusService],
-  exports: [StoresService, MenusService],
-})
+    imports: [UsersModule],
+    controllers: [StoresController, MenusController],
+    providers: [StoresService, MenusService],
+    exports: [StoresService, MenusService],
+    })
 export class StoresModule {}
