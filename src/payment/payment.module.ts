684df9b4bea4296f053829b70710429334e06a7b
dan <dan@payapcorp.com>
2023-06-26 01:41:00 +0900

    🔥 Remove Payment Controller

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
+import { PrismaService } from 'src/prisma';
 
 @Module({
-  controllers: [PaymentController],
-  providers: [PaymentService]
+  providers: [PaymentService, PrismaService],
+  exports:[PaymentService]
   })
 export class PaymentModule {}
