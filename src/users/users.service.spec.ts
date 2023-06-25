2153db62feea5c118efff3a0942e8a984e74a476
ninthsun91 <ninthsun91@gmail.com>
2023-06-25 17:20:56 +0900

    :white_check_mark: [unit] user signup - new Prisma mocking
    - better Prisma mocking by using jest-mock-extended

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,51 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
+import { PrismaClient } from '@prisma/client';
 import { is } from 'typia';
 import { UsersService } from './users.service';
 import { UserType } from 'src/types';
 import { PrismaService } from 'src/prisma';
-import { UserDto } from './dto';
 
 describe('UsersService', () => {
   let service: UsersService;
-
-  interface Select {
-    where: {
-      email: string;
-    };
-  }
-
-  interface Create {
-    data: {
-      email: string;
-      name: string;
-      password: string;
-      type: UserType;
-    }
-  }
+  let mockPrisma: DeepMockProxy<PrismaClient>;
 
   const testUser = {
     userId: 1,
     email: 'test1@delivery.com',
     name: 'Test Kim',
     password: 'qwe1234',
-    type: 'customer',
-  }
-
-  const mockPrismaService = {
-    user: {
-      findUnique: jest.fn(async (select: Select) => {
-        return select.where.email === testUser.email ? testUser : null;
-      }),
-      create: jest.fn(async ({ data }: Create): Promise<UserDto> => {
-        if (data.email === testUser.email) {
-          throw new Error();
-        }
-        return {
-          ...data,
-          userId: 2,
-        }
-      }),
-    }
+    type: UserType.CUSTOMER,
   }
 
   beforeEach(async () => {
@@ -56,10 +26,11 @@ describe('UsersService', () => {
       ],
     })
       .overrideProvider(PrismaService)
-      .useValue(mockPrismaService)
+      .useValue(mockDeep<PrismaClient>())
       .compile();
 
     service = module.get<UsersService>(UsersService);
+    mockPrisma = module.get(PrismaService);
   });
 
   it('should be defined', () => {
@@ -76,34 +47,46 @@ describe('UsersService', () => {
     describe('Check UserType', () => {
       it('should return false if UserType is undefined.', () => {
         const userType = undefined;
+        
         const result = is<UserType>(userType);
+        
         expect(result).toBe(false);
       });
   
       it('should return false if UserType is not a valid UserType.', () => {
         const userType = 'admin';
+        
         const result = is<UserType>(userType);
+        
         expect(result).toBe(false);
       });
   
       it('should return true if UserType is a valid UserType.', () => {
         const userTypes = Object.values(UserType);
         const userType = userTypes[Math.floor(Math.random() * userTypes.length)];
+        
         const result = is<UserType>(userType);
+        
         expect(result).toBe(true);
       });
     });
 
     describe('Check user duplication', () => {
       it('should return a User if user exists.', async () => {
+        mockPrisma.user.findUnique.mockResolvedValueOnce(testUser);
         const where = { email: testUser.email };
+
         const result = await service.findUserByEmail(where);
+
         expect(result).toEqual(testUser);
       });
 
       it('should return null if user does not exist.', async () => {
+        mockPrisma.user.findUnique.mockResolvedValueOnce(null);
         const where = { email: signupForm.email };
+
         const result = await service.findUserByEmail(where);
+
         expect(result).toBe(null);
       });
     });
@@ -115,6 +98,8 @@ describe('UsersService', () => {
           email: testUser.email,
           type: UserType.CUSTOMER,
         };
+        mockPrisma.user.create.mockRejectedValueOnce(new Error());
+        
         await expect(service.createUser(data)).rejects.toThrow();
       });
 
@@ -123,7 +108,14 @@ describe('UsersService', () => {
           ...signupForm,
           type: UserType.CUSTOMER,
         };
+        const newUser = {
+          ...data,
+          userId: 2,
+        }
+        mockPrisma.user.create.mockResolvedValueOnce(newUser);
+
         const result = await service.createUser(data);
+
         expect(result.userId).toBe(2);
         expect(is<User>(result)).toBe(true);
       });
