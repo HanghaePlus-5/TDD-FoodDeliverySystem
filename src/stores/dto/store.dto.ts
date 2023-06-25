4985a36228808fa8ad1b6c65ce6c571471a94626
Louis-Gil <lotus0110@nate.com>
2023-06-25 15:23:03 +0900

    :label: StoreDto, Store Types
    - add StoreDto
    - add StoreType, StoreStatus

---
+++ src/stores/dto/store.dto.ts
@@ -0,0 +1,18 @@
+export interface StoreDto {
+  idx: number;
+  name: string;
+  type: StoreType;
+  status: StoreStatus;
+  businessNumber: string;
+  phoneNumber: string;
+  postalNumber: string;
+  address: string;
+  openingTime: number;
+  closingTime: number;
+  cookingTime: number;
+  reviewNumber: number;
+  averageScore: number;
+  origin: string;
+  description: string;
+  registrationDate: Date;
+}
