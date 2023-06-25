8d015d023413657d4193139b3f1f7baad42fd48a
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2023-06-25 22:22:44 +0900

    :tada: User Module, Controller, Service 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypiaModule } from './typia/typia.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
     UsersModule,
-    TypiaModule
+    TypiaModule,
+    OrdersModule
   ],
   controllers: [],
   providers: [],
