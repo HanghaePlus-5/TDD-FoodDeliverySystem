0a94787d32f39f027aa553f35e77c99a54b562de
ninthsun91 <ninthsun91@gmail.com>
2023-06-26 02:00:29 +0900

    :rotating_light: Run lint - indent spacing

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { UserTypeGuard } from './guards/user-type.guard';
 
 @Module({
-    providers: [UserTypeGuard],
-    exports: [UserTypeGuard],
-    })
+  providers: [UserTypeGuard],
+  exports: [UserTypeGuard],
+  })
 export class AuthModule {}
--- src/auth/guards/user-type.guard.ts
+++ src/auth/guards/user-type.guard.ts
@@ -3,9 +3,9 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class UserTypeGuard implements CanActivate {
-    canActivate(
-        context: ExecutionContext,
-    ): boolean | Promise<boolean> | Observable<boolean> {
-        return true;
-    }
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    return true;
+  }
 }
