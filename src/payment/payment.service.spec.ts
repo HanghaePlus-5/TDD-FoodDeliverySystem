4673db8af6bb330320578825d2ecb0a1cd9458e9
dan <dan@payapcorp.com>
2023-06-26 01:44:11 +0900

    ✅ Add Cancel Request & Inject Prisma Service - Add Failed Test Code - Add Functions - Inject Prisma Service to Payment Service - Inject Mock Prisma Service to Payment Service Spec

--- src/payment/payment.service.spec.ts
+++ src/payment/payment.service.spec.ts
@@ -6,53 +6,77 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentDto } from 'src/payment/dto/payment.dto';
 
 import { PaymentService } from './payment.service';
+import { PrismaService } from 'src/prisma';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { PrismaClient } from '@prisma/client';
 
 describe('PaymentService', () => {
   let service: PaymentService;
+  let mockPrisma: DeepMockProxy<PrismaClient>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PaymentService],
-    }).compile();
-
-    service = module.get<PaymentService>(PaymentService);
+      providers: [PaymentService,PrismaService],
+    })
+    .overrideProvider(PrismaService)
+    .useValue(mockDeep<PrismaClient>())
+    .compile();    service = module.get<PaymentService>(PaymentService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-
+  const paymentDto : PaymentDto = {
+    paymentId : 1,
+    cardExpiryMonth: 6,
+    cardExpiryYear: 2027,
+    cardHolderName: 'michael',
+    cardIssuer: 'abc',
+    cardNumber: '1111-1111-1111-1121',
+  };
+  const orderInfo = {
+    customerName: 'michael',
+  };
   describe('send payment request to payment-gateway', () => {
-    const paymentDto : PaymentDto = {
-      cardExpiryMonth: 6,
-      cardExpiryYear: 2027,
-      cardHolderName: 'michael',
-      cardIssuer: 'abc',
-      cardNumber: '1111-1111-1111-1121',
-    };
-    const orderInfo = {
-      customerName: 'michael',
-    };
-    describe('validate payment-info', () => {
+
+    describe('validate payment request', () => {
       it('should return false if user and card holder name are different', () => {
         const cardHolderName = 'foo';
         const customerName = 'poo';
-        expect(service._validCardHolder(cardHolderName, customerName)).toBe(false);
+        expect(service.validateCardHolder(cardHolderName, customerName)).toBe(false);
       });
       it('should return false with invalid card number format', () => {
         const cardNumber = '4124-1244-4124-414';
-        expect(service._validateCardNumber(cardNumber)).toBe(false);
+        expect(service.validateCardNumber(cardNumber)).toBe(false);
       });
     });
     describe('paymentRequest to PG', () => {
       it('should throw error if failed', () => {
         const mock = { ...paymentDto, cardNumber: '1111-1111-1111-1111' };
-        expect(() => service.sendPaymentRequestToPG(mock, orderInfo.customerName)).toThrow(BadRequestException);
+        expect(() => service.sendPaymentRequestToPG(mock)).toThrow(BadRequestException);
       });
       it('should return ACCEPTED if successful', () => {
         const mock = { ...paymentDto, cardNumber: '1111-1111-1111-1112' };
-        expect(service.sendPaymentRequestToPG(mock, orderInfo.customerName)).toBe(HttpStatus.ACCEPTED);
+        expect(service.sendPaymentRequestToPG(mock)).toBe(HttpStatus.ACCEPTED);
       });
     });
+  });
+
+  describe('cancelRequest to PG', () => {
+    describe('validate cancel request',()=>{
+      it('should return false if order data is not exist', () => {
+        var orderId = '10'
+        expect(service.isExistingOrder()).toBe(false);
+      });
+      it('should return false if order status is not `ORDER ACCEPTED`', () => {
+        var oderStatus = 'ORDER CONFIRMED'
+        expect(service.isOrderStatusAccepted()).toBe(false)
+      });
+      it('should return false if payment status is not `payment completed`', () => {
+        var paymentStatus = 'payment canceled'
+        expect(service.isPaymentStatusCompleted()).toBe(false)
+      });
+    })
+
   });
 });
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,26 +1,65 @@
 import {
   BadRequestException, HttpCode, HttpException, HttpStatus, Injectable,
 } from '@nestjs/common';
-import { TypedBody } from '@nestia/core';
 
 import { PaymentDto } from 'src/payment/dto/payment.dto';
+import { PrismaService } from 'src/prisma';
 
 @Injectable()
 export class PaymentService {
-  sendPaymentRequestToPG(paymentInfo : PaymentDto, customerName:string) {
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
+  
+  //payment request
+  makePayment(paymentInfo : PaymentDto, customerName:string){
     if (!this.validatePaymentInfo(paymentInfo, customerName)) throw new BadRequestException();
+    
+    try {
+    this.sendPaymentRequestToPG(paymentInfo)
+    //create payment data
+    //return PaymentDto or Success
+    } catch (error) {
+      throw new Error(error);
+    }
+
+  }
+  sendPaymentRequestToPG(paymentInfo : PaymentDto) {
     if (paymentInfo.cardNumber == '1111-1111-1111-1111') throw new BadRequestException();
     return HttpStatus.ACCEPTED;
   }
   validatePaymentInfo(paymentInfo : PaymentDto, customerName : string) {
-    return this._validCardHolder(paymentInfo.cardHolderName, customerName)
-            && this._validateCardNumber(paymentInfo.cardNumber);
+    return this.validateCardHolder(paymentInfo.cardHolderName, customerName)
+            && this.validateCardNumber(paymentInfo.cardNumber);
   }
-  _validCardHolder(cardHolderName : string, customerName : string) {
+  validateCardHolder(cardHolderName : string, customerName : string) {
     return cardHolderName == customerName;
   }
-  _validateCardNumber(cardNumber:string) {
+  validateCardNumber(cardNumber:string) {
     cardNumber = cardNumber.replace(/[-\s]/g, '');
     return cardNumber.length == 16;
   }
+
+  //cancel request
+  cancelPayment(){
+    
+  }
+  sendCancelRequestToPG (){
+
+  }
+  validateCancelRequest(){
+    return
+  }
+
+  isExistingOrder(){
+    return 
+  }
+  isOrderStatusAccepted(){
+    return
+  }
+  isPaymentStatusCompleted(){
+    return
+  }
+
+
 }
