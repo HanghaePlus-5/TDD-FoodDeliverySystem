e8dc37b4d2b6eb1b6fae84e5f626aa006d73c042
Louis-Gil <lotus0110@nate.com>
2023-06-25 21:59:16 +0900

    :construction: rename storeId

--- src/stores/dto/store-create.dto.ts
+++ src/stores/dto/store-create.dto.ts
@@ -3,5 +3,5 @@ import { StoreDto } from './store.dto';
 export interface StoreCreateDto
   extends Omit<
     StoreDto,
-    'idx' | 'status' | 'reviewNumber' | 'averageScore' | 'registrationDate'
+    'storeId' | 'status' | 'reviewNumber' | 'averageScore' | 'registrationDate'
   > {}
