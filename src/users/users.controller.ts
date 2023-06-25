6f292c873d39474f2e7830f0092403a12875d801
ninthsun91 <ninthsun91@gmail.com>
2023-06-25 18:43:25 +0900

    :white_check_mark: [integration] user signup - create
    - pass: should throw Error if create fails

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,15 @@
-import { BadRequestException, Controller } from '@nestjs/common';
+import { BadRequestException, Controller, InternalServerErrorException } from '@nestjs/common';
 import { TypedRoute } from '@nestia/core';
 import { is } from 'typia';
 import { UsersService } from './users.service';
+import { UserCreateDto } from './dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @TypedRoute.Post('/signup')
-  async signup(form, query) {
+  async signup(form: UserCreateDto, query) {
     if (!is<UserType>(query.type)) {
       throw new BadRequestException();
     }
@@ -16,6 +17,14 @@ export class UsersController {
     const user = await this.usersService.findUserByEmail({ email: form.email });
     if (is<User>(user)) {
       throw new BadRequestException();
+    }
+
+    const createdUser = await this.usersService.createUser({
+      ...form,
+      type: query.type,
+    });
+    if (!is<User>(createdUser)) {
+      throw new InternalServerErrorException();
     }
 
     return true;
