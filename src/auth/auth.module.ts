08100c9dff8eebdf20373c1a1efbf0952cf002dc
ninthsun91 <ninthsun91@gmail.com>
2023-06-27 01:49:35 +0900

    :sparkles: Add auth module templates
    - create template files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { UserTypeGuard } from './guards/user-type.guard';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({})
   ],
-  providers: [UserTypeGuard],
+  providers: [UserTypeGuard, AuthService],
   exports: [UserTypeGuard],
   })
 export class AuthModule {}
---
+++ src/auth/passport/jwt.stategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { is } from 'typia';
+import { EnvService } from 'src/config/env';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly env: EnvService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: env.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any): Promise<UserPayload> {
+    if (is<UserPayload>(payload)) {
+      return { ...payload };
+    }
+    throw new UnauthorizedException('Invalid token payload');
+  }
+}
