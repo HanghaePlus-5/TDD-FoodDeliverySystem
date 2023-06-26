f679e33ecc491454a6e7f8da101c31956c52bb78
ninthsun91 <ninthsun91@gmail.com>
2023-06-27 02:09:30 +0900

    :white_check_mark: [unit] jwt auth > create access token > invalid
    - pass: should return null if invalid payload

--- src/auth/services/jwt-auth.service.ts
+++ src/auth/services/jwt-auth.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { is } from 'typia';
 
 @Injectable()
 export class JwtAuthService {
   constructor() {}
 
   createAccessToken(user) {
+    const payload = this.createUserPayload(user);
+    if (!is<UserPayload>(payload)) {
+      return null;
+    }
     return true;
+  }
+
+  private createUserPayload(user: User) {
+    return {
+      userId: user.userId,
+      name: user.name,
+      type: user.type,
+    }
   }
 }
