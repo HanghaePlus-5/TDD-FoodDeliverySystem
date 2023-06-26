a89da22922e92d925155a247ba8e0fc9b4221636
ninthsun91 <ninthsun91@gmail.com>
2023-06-27 01:59:54 +0900

    :test_tube: [unit] jwt auth - list test cases
    - create access token
    - verify access token

--- src/auth/services/jwt-auth.service.spec.ts
+++ src/auth/services/jwt-auth.service.spec.ts
@@ -16,4 +16,17 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('Create Access Token', () => {
+    it.todo('should return null if invalid user payload.');
+    it.todo('should return null if fail to create token.');
+    it.todo('should return jwt token if success.');
+  });
+
+  describe('Verify Access Token', () => {
+    it.todo('should return false if expired token.');
+    it.todo('should return false if mismatch secert key.');
+    it.todo('should return false if invalid user payload.');
+    it.todo('should return UserPayload if success.');
+  });
 });
