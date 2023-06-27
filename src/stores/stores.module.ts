4e7afeba3d147aa75bb98628f16bbb6731ed50e5
Louis-Gil <lotus0110@nate.com>
2023-06-27 09:02:15 +0900

    :sparkles: add prisma at stores

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -4,11 +4,12 @@ import { MenusController } from './menus/menus.controller';
 import { MenusService } from './menus/menus.service';
 import { StoresController } from './stores/stores.controller';
 import { StoresService } from './stores/stores.service';
+import { PrismaService } from 'src/prisma';
 
 @Module({
   imports: [],
   controllers: [StoresController, MenusController],
-  providers: [StoresService, MenusService],
+  providers: [StoresService, MenusService, PrismaService],
   exports: [StoresService, MenusService],
 })
 export class StoresModule {}
