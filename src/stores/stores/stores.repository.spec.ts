11622503888d213e575672aace34bb53882baae1
Louis-Gil <lotus0110@nate.com>
2023-06-27 19:54:31 +0900

    :test_tube: add test name already exists
    - add sampleCreateStoreDto sampleStoreDto
    - add should throw error name alreay exists

--- src/stores/stores/stores.repository.spec.ts
+++ src/stores/stores/stores.repository.spec.ts
@@ -8,10 +8,44 @@ import { EnvService } from 'src/config/env';
 
 import { StoresRepository } from './stores.repository';
 import { StoresService } from './stores.service';
+import { StoreCreateDto, StoreDto } from '../dto';
 
 describe('StoresRepository', () => {
-  let Repository: StoresRepository;
+  let repository: StoresRepository;
   let mockPrisma: DeepMockProxy<PrismaClient>;
+
+  const sampleCreateStoreDto: StoreCreateDto = {
+    name: '커피커피',
+    type: 'CAFE',
+    businessNumber: '783-86-01715',
+    phoneNumber: '02-1234-1234',
+    postalNumber: '06210',
+    address: '서울 강남구 테헤란로44길 8 12층(아이콘역삼빌딩)',
+    openingTime: 9,
+    closingTime: 22,
+    cookingTime: 10,
+    origin: '커피원두(국내산), 우유(국내산)',
+    description: '코딩이 맛있어요!',
+  };
+
+  const sampleStoreDto: StoreDto = {
+    storeId: 1,
+    name: '커피커피',
+    type: 'CAFE',
+    status: 'OPEN',
+    businessNumber: '783-86-01715',
+    phoneNumber: '02-1234-1234',
+    postalNumber: '06210',
+    address: '서울 강남구 테헤란로44길 8 12층(아이콘역삼빌딩)',
+    openingTime: 9,
+    closingTime: 22,
+    cookingTime: 10,
+    reviewNumber: 0,
+    averageScore: 0,
+    origin: '커피원두(국내산), 우유(국내산)',
+    description: '코딩이 맛있어요!',
+    registrationDate: new Date(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,11 +55,22 @@ describe('StoresRepository', () => {
       .overrideProvider(PrismaService)
       .useValue(mockDeep<PrismaService>())
       .compile();
-    Repository = module.get<StoresRepository>(StoresRepository);
+    repository = module.get<StoresRepository>(StoresRepository);
     mockPrisma = module.get(PrismaService);
   });
 
   it('should be defined', () => {
-    expect(Repository).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should throw an error if store name already exists', async () => {
+      const mockfindOne = jest.spyOn(repository, 'findOne');
+      mockfindOne.mockResolvedValue(sampleStoreDto);
+
+      await expect(
+        repository.create(sampleCreateStoreDto)
+      ).rejects.toThrowError('Store name already exists');
+    });
   });
 });
