2fdade03412acb33c5da64f0a9b08fcfb0ea226f
ninthsun91 <ninthsun91@gmail.com>
2023-06-29 06:31:36 +0900

    :test_tube: [unit] user guard - add test cases
    - initial test setting
    - check user payload
    - check user type

--- src/auth/guards/user-type.guard.spec.ts
+++ src/auth/guards/user-type.guard.spec.ts
@@ -1,7 +1,54 @@
+import { ExecutionContext } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import { UserTypeGuard } from './user-type.guard';
+import { UserType } from 'src/types';
 
 describe('AuthGuard', () => {
+  let guard: UserTypeGuard;
+
+  const testUserPayload: UserPayload = {
+    userId: 1,
+    name: 'Test Kim',
+    type: UserType.CUSTOMER,
+  };
+
+  const createContext = (users: UserPayload) => ({
+    getHandler: jest.fn(),
+    getClass: jest.fn(),
+    switchToHttp: jest.fn(() => ({
+      getRequest: jest.fn(() => ({
+        users: {},
+      })),
+      getResponse: jest.fn(),
+    })),
+  } as any) as ExecutionContext;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [],
+      providers: [
+        UserTypeGuard
+      ],
+    }).compile();
+
+    guard = module.get<UserTypeGuard>(UserTypeGuard);
+  });
+
   it('should be defined', () => {
-    expect(new UserTypeGuard()).toBeDefined();
+    expect(guard).toBeDefined();
+  });
+
+  describe('check UserPayload.', () => {
+    it.todo('should throw Error if Request.user is empty.');
+
+    it.todo('should throw Error if invalid UserPayload.');
+
+    it.todo('should return true if valid UserPayload.');
+  });
+
+  describe('check UserType.', () => {
+    it.todo('should throw Error if invalid UserType.');
+
+    it.todo('should return true if valid UserType.');
   });
 });
