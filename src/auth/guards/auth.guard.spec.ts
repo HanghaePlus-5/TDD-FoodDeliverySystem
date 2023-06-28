5fe6bb95778766036b3f3e984b152a10e29b28c3
ninthsun91 <ninthsun91@gmail.com>
2023-06-29 01:37:53 +0900

    :white_check_mark: Set testing module for AuthGuard

--- src/auth/guards/auth.guard.spec.ts
+++ src/auth/guards/auth.guard.spec.ts
@@ -1,7 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
+  let guard: AuthGuard;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AuthGuard],
+    }).compile();
+
+    guard = module.get<AuthGuard>(AuthGuard);
+  });
+
   it('should be defined', () => {
-    expect(new AuthGuard()).toBeDefined();
+    expect(guard).toBeDefined();
   });
 });
