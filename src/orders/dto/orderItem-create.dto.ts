6623b814581776951e61b86d131da049cf6b9cf4
jkim9409 <jkimnyc2020@gmail.com>
2023-06-28 17:11:50 +0900

    :label: Modify payment schema, order schema, orderItemDTO

--- src/orders/dto/orderItem-create.dto.ts
+++ src/orders/dto/orderItem-create.dto.ts
@@ -1,3 +1,4 @@
 import { OrderItemDto } from "./orderItem.dto";
 
-export type OrderItemCreateDto = Pick<OrderItemDto, 'orderId'|'quantity'|'menuId'>
+export type OrderItemCreateDto = Pick<OrderItemDto, 'quantity'|'menuId'>
+export type OrderItemCreatePrismaDto = Pick<OrderItemDto, 'orderId'|'quantity'|'menuId'>
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma';
-import { Prisma, Order} from '@prisma/client';
+import { Prisma, Order, OrderItem} from '@prisma/client';
 import { OrderCreateDto } from './dto/order-create.dto';
-import { OrderItemCreateDto } from './dto/orderItem-create.dto';
+import { OrderItemCreateDto, OrderItemCreatePrismaDto } from './dto/orderItem-create.dto';
 
 
 @Injectable()
@@ -10,35 +10,52 @@ export class OrdersService {
     constructor(
         private readonly prisma: PrismaService,
     ) {}
-    
+
     async createOrder( orderCreateDto: OrderCreateDto
     ){
         this.isUserTypeCustomer(orderCreateDto.user.userId);
         this.isOrderItemCountInRange(orderCreateDto.orderItem);
+        const orderItemList : OrderItemCreatePrismaDto[] = [];
+        let orderTotalPrice: number = 0
+
 
         if (orderCreateDto.orderItem) {
-            if (Array.isArray(orderCreateDto.orderItem)) {
-              for (const orderItem of orderCreateDto.orderItem) {
-                this.isValidMenu(orderItem);
-                this.hasEnoughStock(orderItem);
-              }
-            } else {
-              this.isValidMenu(orderCreateDto.orderItem);
-              this.hasEnoughStock(orderItem);
+            for (const orderItem of orderCreateDto.orderItem) {
+            this.isValidMenu(orderItem);
+            this.hasEnoughStock(orderItem);
+            
+
             }
-          } else {
+        } else {
             new Error()
-          }
+        }
+
 
         this.isValidStore(orderCreateDto.storeId);
         this.hasOngoingOrder(orderCreateDto.user.userId);
-        const savedOrder = await this.saveOrder(orderCreateDto);
+        const order = {
+            userId:orderCreateDto.user.userId,
+            storeId:orderCreateDto.storeId
+        }
+        const savedOrder = await this.saveOrder(order);
         const orderId = savedOrder.orderId;
-        const savedOrderItem = await this.saveOrderItem(orderCreateDto.orderItem);
+       
+        
+        for (const orderItem of orderCreateDto.orderItem) {
+            const orderItemData : OrderItemCreatePrismaDto = {
+                orderId,
+                menuId:orderItem.menuId,
+                quantity: orderItem.quantity
+            } 
+            orderItemList.push(orderItemData);
+            }
+
+        const savedOrderItem = await this.saveOrderItemList(orderItemList);
         const processedOrder = { ...savedOrder, orderId } as Order;
         this.processPayment(processedOrder);
         this.alarmStoreInitially(processedOrder);
-        // 주문번호, 주문상태 ,  
+
+        // 주문번호, 주문상태  
         const result = processedOrder
         return result
     }
@@ -48,9 +65,7 @@ export class OrdersService {
     isValidStore(storeId: number) {
         throw new Error('Method not implemented.');
     }
-    saveOrderItem(orderItem: Prisma.OrderItemUncheckedCreateNestedManyWithoutOrderInput | undefined) {
-        throw new Error('Method not implemented.');
-    }
+
     alarmStoreInitially(processedOrder: Order) {
         throw new Error('Method not implemented.');
     }
@@ -60,14 +75,16 @@ export class OrdersService {
     hasEnoughStock(orderItem: OrderItemCreateDto) {
         throw new Error('Method not implemented.');
     }
-    isOrderItemCountInRange(orderItem: Prisma.OrderItemUncheckedCreateNestedManyWithoutOrderInput[] | Prisma.OrderItemCreateManyInput[] | Prisma.OrderItemUncheckedCreateNestedManyWithoutOrderInput | undefined | Prisma.OrderItemCreateManyInput[]) {
+    isOrderItemCountInRange(orderItem: OrderItemCreateDto[]) {
         throw new Error('Method not implemented.');
     }
     saveOrder( data: Prisma.OrderUncheckedCreateInput): Prisma.Prisma__OrderClient<Order, never>
         {
             return this.prisma.order.create({data});
         }
-
+    saveOrderItemList(data: OrderItemCreatePrismaDto[]) {
+        return this.prisma.orderItem.createMany({data})
+    }
     processPayment(order: Order) {
     }
 
@@ -89,20 +106,3 @@ export class OrdersService {
     
     
 }
-
-    // addOrder(order:CustomOrder, user: User): number {
-    //     if (!this.isUserTypeCustomer(user)) {
-    //         throw new Error('Only customers are allowed to add orders.');
-    //       }
-    //     if(this.userHasOngoingOrder(user)){
-    //         throw new Error('User already has an ongoing order.');
-    //     }
-    //     this.processPayment(order);
-    //     this.Orders.push(order);
-        
-    //     return order.id
-    // }
-    
-    // userHasOngoingOrder(user: User): boolean {
-    //     return this.Orders.some(order => order.userId === user.userId);
-    // }
