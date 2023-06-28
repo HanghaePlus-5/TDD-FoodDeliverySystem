3e3c8f13806211009ff7ac087d9e4021763f08c5
ninthsun91 <ninthsun91@gmail.com>
2023-06-29 01:43:55 +0900

    :test_tube: Add test cases for AuthGuard
    - verify access token
    - prepare mocked execution context

--- src/auth/guards/auth.guard.spec.ts
+++ src/auth/guards/auth.guard.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthGuard } from './auth.guard';
+import { ExecutionContext } from '@nestjs/common';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
@@ -14,5 +15,23 @@ describe('AuthGuard', () => {
 
   it('should be defined', () => {
     expect(guard).toBeDefined();
+  });
+
+  describe('verify access token.', () => {
+    const context: ExecutionContext = {
+      switchToHttp: jest.fn(() => ({
+        getRequest: jest.fn(),
+      })),
+    } as any;
+
+    it.todo('should return false if no access token.');
+
+    it.todo('should return false if expired access token.');
+
+    it.todo('should return false if wrong signed access token.');
+
+    it.todo('should return false if invalid user payload.');
+
+    it.todo('should return true if valid access token.');
   });
 });
