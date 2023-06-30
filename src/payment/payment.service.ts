535c91f88e4e8cc7428f627dc0cb4f7f3e1f2cce
dan <dan@payapcorp.com>
2023-06-30 19:59:05 +0900

    ♻️ Refactor payment service

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -8,6 +8,7 @@ import { PaymentGatewayService } from 'src/lib/payment-gateway/payment-gateway.s
 
 import { PaymentCreateDto } from './dto/payment-create.dto';
 import { validatePaymentStatus, validateCardHolder, validateCardNumber } from './func';
+import { Payment, PaymentStatus } from '@prisma/client';
 
 @Injectable()
 export class PaymentService {
@@ -16,26 +17,22 @@ export class PaymentService {
     private readonly pgService: PaymentGatewayService,
   ) { }
   // payment request
-  async makePayment(data: PaymentCreateDto, orderDto: any) {
-    validateCardHolder(data.cardHolderName, orderDto.customerName)
-    validateCardNumber(data.cardNumber)
-    const response = await this.pgService.sendPaymentRequestToPG(data);
-    if (response.status === HttpStatus.ACCEPTED) {
-      const payment = await this.prisma.payment.create({ data });
-      return payment;
-    }
+  async makePayment(paymentCreateDto: PaymentCreateDto, orderDto: any): Promise<Payment> {
+    validateCardHolder(paymentCreateDto.cardHolderName, orderDto.customerName);
+    validateCardNumber(paymentCreateDto.cardNumber);
+    const response = await this.pgService.sendPaymentRequestToPG(paymentCreateDto);
+    const data = { ...paymentCreateDto, paymentGatewayId: response.data.paymentGatewayId }
+    const payment = await this.prisma.payment.create({ data });
+    return payment;
   }
 
   // cancel request
-  async cancelPayment(paymentId: number) {
+  async cancelPayment(paymentId: number): Promise<Payment> {
     const payment = await this.prisma.payment.findUnique({ where: { paymentId } });
     if (!payment) throw new BadRequestException('payment not exist');
-    validatePaymentStatus(payment.paymentStatus)
-    const response = await this.pgService.sendCancelRequestToPG(payment.paymentGatewayId);
-    if (response.status === HttpStatus.ACCEPTED) {
-      // payment status update
-      return payment;
-    }
-
+    validatePaymentStatus(payment.paymentStatus);
+    await this.pgService.sendCancelRequestToPG(payment.paymentGatewayId);
+    const updatedPayment = await this.prisma.payment.update({ where: { paymentId }, data: { paymentStatus: PaymentStatus.canceled } })
+    return updatedPayment;
   }
 }
