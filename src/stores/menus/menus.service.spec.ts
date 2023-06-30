96c507226135d51e9855045e021f2120b42ea321
Louis-Gil <lotus0110@nate.com>
2023-06-30 20:03:23 +0900

    :test_tube: menus service tset init

--- src/stores/menus/menus.service.spec.ts
+++ src/stores/menus/menus.service.spec.ts
@@ -1,19 +1,41 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { mockDeep } from 'jest-mock-extended';
 
+import { PrismaService } from 'src/prisma';
+import { EnvService } from 'src/config/env';
+
+import { MenusRepository } from './menus.repository';
 import { MenusService } from './menus.service';
+import { StoresService } from '../stores/stores.service';
 
 describe('MenusService', () => {
-  let service: MenusService;
+  let menusService: MenusService;
+  let storesService: StoresService;
+  let envService: EnvService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MenusService],
-    }).compile();
+      imports: [ConfigModule.forRoot()],
+      providers: [
+        MenusService,
+        StoresService,
+        MenusRepository,
+        EnvService,
+        PrismaService,
+      ],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile();
 
-    service = module.get<MenusService>(MenusService);
+    menusService = module.get<MenusService>(MenusService);
+    storesService = module.get<StoresService>(StoresService);
+    envService = module.get<EnvService>(EnvService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(menusService).toBeDefined();
   });
 });
