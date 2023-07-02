53c99800455cab12afb89e2e7ac28655ad79dafe
Louis-Gil <lotus0110@nate.com>
2023-07-02 16:21:56 +0900

    :white_check_mark: update menu add exec checkStoreStatusGroup

--- src/stores/menus/menus.service.ts
+++ src/stores/menus/menus.service.ts
@@ -5,6 +5,7 @@ import { ACTIVATE_MENU_STATUES } from 'src/constants/stores';
 import { MenusRepository } from './menus.repository';
 import { MenuCreateDto } from '../dto/menu-create.dto';
 import { StoresService } from '../stores/stores.service';
+import { MenuUpdateDto } from '../dto/menu-update.dto';
 
 @Injectable()
 export class MenusService {
@@ -43,6 +44,24 @@ export class MenusService {
       return menu;
     } catch (error) {
       throw new Error('Menu creation failed.');
+    }
+  }
+
+  async updateMenu(userId: number, dto: MenuUpdateDto) {
+    const isStoreOwned = await this.storesService.checkStoreOwned({
+      userId,
+      storeId: dto.storeId,
+    });
+    if (!isStoreOwned) {
+      throw new Error('User does not own store');
+    }
+    
+    const isStoreStatusGroup = await this.storesService.checkStoreStatusGroup(
+      isStoreOwned.status,
+      ['REGISTERED', 'OPEN', 'CLOSED'],
+    );
+    if (!isStoreStatusGroup) {
+      throw new Error('Store status is not allowed.');
     }
   }
 }
