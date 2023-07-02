35b13335e3dedfbc25a4ac2b5287cf3d5f3ce277
Louis-Gil <lotus0110@nate.com>
2023-07-03 00:23:05 +0900

    :white_check_mark: add createSampleStoreMenuDto

--- src/stores/test/utils/testUtils.ts
+++ src/stores/test/utils/testUtils.ts
@@ -1,6 +1,7 @@
 import { MenuDto, StoreCreateDto, StoreDto } from 'src/stores/dto';
 import { MenuCreateDto } from 'src/stores/dto/menu-create.dto';
 import { MenuUpdateDto } from 'src/stores/dto/menu-update.dto';
+import { StoreMenuDto } from 'src/stores/dto/store-menu.dto';
 import { StoreUpdateDto } from 'src/stores/dto/store-update.dto';
 
 export function createSampleCreateStoreDto(dto: Partial<StoreCreateDto>): StoreCreateDto {
@@ -96,4 +97,33 @@ export function createSampleMenuDto(dto: Partial<MenuDto>): MenuDto {
     storeId: 1,
     ...dto,
   };
+}
+
+export function createSampleStoreMenuDto(dto: Partial<StoreMenuDto>): StoreMenuDto {
+  return {
+    storeId: 1,
+    name: '커피커피',
+    type: 'CAFE',
+    status: 'OPEN',
+    businessNumber: '783-86-01715',
+    phoneNumber: '02-1234-1234',
+    postalNumber: '06210',
+    address: '서울 강남구 테헤란로44길 8 12층(아이콘역삼빌딩)',
+    openingTime: 9,
+    closingTime: 22,
+    cookingTime: 10,
+    reviewNumber: 0,
+    averageScore: 0,
+    origin: '커피원두(국내산), 우유(국내산)',
+    description: '코딩이 맛있어요!',
+    menus: [{
+      menuId: 1,
+      name: '아메리카노',
+      sort: 1,
+      status: 'OPEN',
+      price: 4000,
+      inventory: 100,
+      description: '커피의 정석',
+    }],
+  }
 }
