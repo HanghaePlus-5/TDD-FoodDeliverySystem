6dc38027f405e23db7f79877ed8689c7197462b1
Louis-Gil <lotus0110@nate.com>
2023-07-02 16:15:31 +0900

    :label: store dto index update

--- src/stores/dto/index.ts
+++ src/stores/dto/index.ts
@@ -1,3 +1,9 @@
 export * from './store.dto';
 export * from './store-create.dto';
+export * from './store-update.dto';
+export * from './store-change-status.dto';
+export * from './store-search.dto';
+
 export * from './menu.dto';
+export * from './menu-create.dto';
+export * from './menu-update.dto';
