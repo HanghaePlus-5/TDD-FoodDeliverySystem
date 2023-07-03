8dc89a2a6d665df497fb4056ea79f6003ba804c2
dan <dan@payapcorp.com>
2023-07-03 23:37:35 +0900

    ✨ Add Delivery Module

--- src/delivery/delivery.module.ts
+++ src/delivery/delivery.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { DeliveryController } from './delivery.controller';
 import { DeliveryService } from './delivery.service';
+import { PrismaService } from 'src/prisma';
 
 @Module({
   controllers: [DeliveryController],
-  providers: [DeliveryService]
+  providers: [DeliveryService, PrismaService],
+  exports: [PrismaService]
 })
-export class DeliveryModule {}
+export class DeliveryModule { }
