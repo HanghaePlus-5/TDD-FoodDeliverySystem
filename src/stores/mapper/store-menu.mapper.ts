75fc8cca4b5f4442ce44aaa205b217c2b1171484
Louis-Gil <lotus0110@nate.com>
2023-07-03 22:14:16 +0900

    :sparkles: add StoreMenuDto mapper

--- src/stores/mapper/store-menu.mapper.ts
+++ src/stores/mapper/store-menu.mapper.ts
@@ -2,6 +2,27 @@ import { Menu, Store } from '@prisma/client';
 
 import { MenuViewDto, StoreMenuDto } from '../dto/store-menu.dto';
 
+export function StoreMenuDtoMap(store: Store, menu: Menu[]): StoreMenuDto {
+  return {
+    storeId: store.storeId,
+    name: store.name,
+    type: store.type,
+    status: store.status,
+    businessNumber: store.businessNumber,
+    phoneNumber: store.phoneNumber,
+    postalNumber: store.postalNumber,
+    address: store.address,
+    openingTime: store.openingTime,
+    closingTime: store.closingTime,
+    cookingTime: store.cookingTime,
+    reviewNumber: store.reviewNumber,
+    averageScore: store.averageScore,
+    origin: store.origin ? store.origin : '',
+    description: store.description ? store.description : '',
+    menus: menu.map(MenuToViewDtoMap),
+  };
+}
+
 export function StoreMenuSearchDtoMap(store: (Store & {menu: Menu[];})): StoreMenuDto {
   return {
     storeId: store.storeId,
