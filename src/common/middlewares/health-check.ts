d5c37db0dac203b2fa2ff1221b4a8e556ca311dd
Louis-Gil <lotus0110@nate.com>
2023-07-11 19:27:56 +0900

    :rotating_light: Run lint

--- src/common/middlewares/health-check.ts
+++ src/common/middlewares/health-check.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 
 export const healthCheckMiddleware = (req: Request, res: Response, next: NextFunction) => {
   if (req.url === '/') {
-    res.sendStatus(200);      
+    res.sendStatus(200);
   } else {
     next();
   }
-}
+};
