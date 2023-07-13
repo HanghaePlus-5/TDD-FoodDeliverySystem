4f22ab3a32bc2274a42255441e40578f402b48a5
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2023-07-13 18:27:34 +0900

    :white_check_mark: check AWS CD

--- src/common/middlewares/health-check.ts
+++ src/common/middlewares/health-check.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 export const healthCheckMiddleware = (req: Request, res: Response, next: NextFunction) => {
   if (req.url === '/') {
-    res.sendStatus(200);      
+    res.send('dev-env');      
   } else {
     next();
   }
