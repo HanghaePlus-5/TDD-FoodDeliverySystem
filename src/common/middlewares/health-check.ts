2387087e0826f9cd80c5891df396cb90af78383d
Louis-Gil <lotus0110@nate.com>
2023-07-15 09:47:25 +0900

    :recycle: Modify HealthCheck boolean appmodule

--- src/common/middlewares/health-check.ts
+++ src/common/middlewares/health-check.ts
@@ -4,7 +4,7 @@ import { AppModule } from 'src/app.module';
 
 export const healthCheckMiddleware = (req: Request, res: Response, next: NextFunction) => {
   if (req.url === '/') {
-    if (AppModule) {
+    if (!Boolean(AppModule)) {
       res.status(500).send('AppModule not found');
     } else {
       res.status(200).send('prod-env-test');
