421d69d48cca8c11570abc298ab4f3679a854e26
Louis-Gil <lotus0110@nate.com>
2023-07-16 14:21:43 +0900

    :label: Add Log type

---
+++ src/types/log.ts
@@ -0,0 +1,19 @@
+interface RequestApiLog {
+  Session: string;
+  Request: string;
+  Headers: string[];
+  Body: string;
+}
+
+interface ResponseApiLog {
+  Session: string;
+  Response: string;
+  Headers: string[];
+  Body: string;
+}
+
+interface ErrorApiLog {
+  Session: string;
+  Message: string;
+  Stack: string;
+}
