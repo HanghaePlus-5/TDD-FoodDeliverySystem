b48a923e7c3686d6af3c5f8074cb0bab1e55ae7b
jkim9409 <jkimnyc2020@gmail.com>
2023-07-19 09:18:54 +0900

    :sparkling_heart: Create order test controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { IgnoreAuth } from 'src/auth/decorators';
 
+@IgnoreAuth()
 @Controller('orders')
 export class OrdersController {
     @Get()
