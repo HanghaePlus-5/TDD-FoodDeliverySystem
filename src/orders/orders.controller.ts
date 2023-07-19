56b7586611aa628212c41a2306986a5b6d7de93a
ninthsun91 <ninthsun91@gmail.com>
2023-07-20 04:04:22 +0900

    :sparkles: Add create order endpoint
    - POST /orders
    - apply @UserTypes('CUSTOMER')

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { TypedBody, TypedRoute } from '@nestia/core';
+import { BadRequestException, Controller, Get, Req } from '@nestjs/common';
+import { OrderCreateDto } from './dto/order-create.dto';
+import { OrdersService } from './orders.service';
+import { UserTypes } from 'src/auth/decorators';
 
 @Controller('orders')
 export class OrdersController {
+    constructor(
+        private readonly ordersService: OrdersService,
+    ) {}
+
     @Get()
     findAll(): string {
         return 'hello';
+    }
+
+    @UserTypes('CUSTOMER')
+    @TypedRoute.Post('/')
+    async create(
+        @Req() req: Express.Request,
+        @TypedBody() body: OrderCreateDto,
+    ) {
+        if (req.payload.userId !== body.user.userId) {
+            throw new BadRequestException();
+        }
+
+        return this.ordersService.createOrder(body);
     }
 }
