d6d8c8468c9feed999c737440896b13206a8ccf4
Louis-Gil <lotus0110@nate.com>
2023-07-20 00:43:44 +0900

    :sparkles: Add create menu controller

--- src/stores/menus/menus.controller.ts
+++ src/stores/menus/menus.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+ Controller, UseGuards, Request, BadRequestException,
+} from '@nestjs/common';
+import { TypedBody, TypedRoute } from '@nestia/core';
+import { is } from 'typia';
+
+import { BearerAuthGuard } from 'src/auth/guards';
+import { ResponseForm, createResponse } from 'src/utils/createResponse';
+
+import { MenusService } from './menus.service';
+import { MenuCreateDto, MenuDto } from '../dto';
 
 @Controller('menus')
-export class MenusController {}
+export class MenusController {
+  constructor(
+    private readonly menusService: MenusService,
+  ) {}
+
+  @TypedRoute.Post('/')
+  @UseGuards(BearerAuthGuard)
+  async createMenu(
+    @Request() req: Express.Request,
+    @TypedBody() form: MenuCreateDto,
+  ): Promise<ResponseForm<MenuDto>> {
+    if (!is<MenuCreateDto>(form)) {
+      throw new BadRequestException();
+    }
+
+    const menu = await this.menusService.createMenu(req.payload, form);
+
+    return createResponse<MenuDto>(menu);
+  }
+}
