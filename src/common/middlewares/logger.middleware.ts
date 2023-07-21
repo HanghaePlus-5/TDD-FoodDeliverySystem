08a8caa46a6aab4ca5461ae0803ea48e5d6dfec2
인식 <110698263+Louis-Gil@users.noreply.github.com>
2023-07-21 23:22:56 +0900

    Feature/logger time error (#52)
    * :wrench: Install session lib
    
    * :sparkles: Add session at main
    
    * :rotating_light: lint error at health-check
    
    * :sparkles: Add logging with session
    
    * :sparkles: Add logging with session
    
    * :zap: Add log healthCheck
    
    * :label: Add Log type
    
    * :sparkles: Add log masking
    
    * :sparkles: Add CustomApiLog
    
    * :test_tube: Refactor Store mo apply userpayload
    
    * :recycle: Refactor Store mo apply test
    
    * :sparkles: Apply createResponse on stores controller
    
    * :wastebasket: Remove no test
    
    * :sparkles: Add update store co
    
    * :sparkles: Add changeStoreStatus controller"
    
    * :sparkles: Add getMyBusinss controller
    
    * :sparkles: Add searchStores controller
    
    * :sparkles: Add create menu controller
    
    * :sparkles: Add menus controllers
    
    * :rotating_light: Run lint
    
    * :sparkles: Add logger warn
    
    * :sparkles: Split logger level at logGroupName
    
    * :sparkles: Add message masking
    
    * :white_check_mark: Fix fail test DI problem
    
    * :wrench: Install uuid Remove session
    
    * :sparkles: Replace session to userId and uuid
    
    * :recycle: Refactor logger to with key,value
    
    * :sparkles: Add err log Identify
    
    * :sparkles: Add custom log masking key,value
    
    * :rotating_light: Run lint and for to forEach
    
    * :recycle: Refactor store guard to custom decorator
    
    * :sparkles: Add stores getStore cont
    
    * :rotating_light: Run lint
    
    * Merge branch 'develop' of github.com:HanghaePlus-5/TDD-FoodDeliverySystem into feature/stores
    
    * :white_check_mark: Fix failed Test by DI
    
    * :sparkles: Modify logger injectable
    
    * :recycle: Rename loggerMiddleware
    
    * :sparkles: Add loggerTimeError

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
 import Logger from 'src/lib/winston/logger';
+import { ServiceUnavailableException } from '@nestjs/common';
 
 export const loggerMiddleware = (
   loggerInstance: Logger,
@@ -17,27 +18,39 @@ export const loggerMiddleware = (
   const start = Date.now();
   const Identify = payload && payload.userId ? String(payload.userId) : req.identify || uuidv4();
 
-  loggerInstance.info({
-    Identify,
-    Request: `${method} ${originalUrl}`,
-    Headers: res.getHeaders(),
-    Body: JSON.stringify(body),
-  });
+  // loggerInstance.info({
+  //   Identify,
+  //   Request: `${method} ${originalUrl}`,
+  //   Headers: res.getHeaders(),
+  //   Body: JSON.stringify(body),
+  // });
 
-  const oldSend = res.send.bind(res);
-  // eslint-disable-next-line
-  res.send = <T>(data: T): Response<T> => {
-    const responseData = oldSend.call(res, data);
+  // const oldSend = res.send.bind(res);
+  // // eslint-disable-next-line
+  // res.send = <T>(data: T): Response<T> => {
+  //   const responseData = oldSend.call(res, data);
 
-    loggerInstance.info({
-      Identify,
-      Response: `${method} ${originalUrl} ${res.statusCode} ${Date.now() - start}ms`,
-      Headers: res.getHeaders(),
-      Body: JSON.stringify(data),
-    });
+  //   loggerInstance.info({
+  //     Identify,
+  //     Response: `${method} ${originalUrl} ${res.statusCode} ${Date.now() - start}ms`,
+  //     Headers: res.getHeaders(),
+  //     Body: JSON.stringify(data),
+  //   });
 
-    return responseData;
-  };
+  //   return responseData;
+  // };
+  
+  res.on('finish', () => {
+    if (Date.now() - start > 1000) {
+      loggerInstance.error({
+        Identify,
+        Request: `${method} ${originalUrl}`,
+        StatusCode: res.statusCode,
+        Message: 'Request timeout',
+        Stack: '',
+      });
+    }
+  })
 
   next();
 };
