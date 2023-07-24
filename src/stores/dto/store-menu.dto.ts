3205fde4ac79442925a39f0e610aae1b52129f34
ninthsun91 <ninthsun91@gmail.com>
2023-07-24 21:33:28 +0900

    :bug: Update store search response dto
    - response dto does not match return data

--- src/stores/dto/store-menu.dto.ts
+++ src/stores/dto/store-menu.dto.ts
@@ -1,7 +1,21 @@
 import { MenuDto } from './menu.dto';
-import { StoreDto } from './store.dto';
 
-export type StoreMenuDto = Omit<StoreDto, 'registrationDate' | 'userId'> & {
-  menus: MenuViewDto[];
+export type StoreMenuDto = {
+  storeId: number;
+  name: string;
+  type: StoreType;
+  status: StoreStatus;
+  businessNumber: string;
+  phoneNumber: string;
+  postalNumber: string;
+  address: string;
+  openingTime: number;
+  closingTime: number;
+  cookingTime: number;
+  reviewNumber: number;
+  averageScore: number;
+  origin: string;
+  description: string;
+  menus: unknown[];
 }
 export type MenuViewDto = Omit<MenuDto, 'registrationDate' | 'storeId'>
